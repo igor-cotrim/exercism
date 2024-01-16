6e53a0ee9d4d30dcc109c90b3193c8c9b85fba03
Igor Cotrim Santos <igorcotrim.dev@gmail.com>
2024-01-15 21:11:42 -0300

    feat: :sparkles: resolve election-day question

--- go/learning-exercise/gross-store.go
+++ go/learning-exercise/gross-store.go
@@ -5,11 +5,11 @@ package learningexercise
 func Units() map[string]int {
 	return map[string]int{
 		"quarter_of_a_dozen": 3,
-		"half_of_a_dozen": 6,
-		"dozen": 12,
-		"small_gross": 120,
-		"gross": 144,
-		"great_gross": 1728,
+		"half_of_a_dozen":    6,
+		"dozen":              12,
+		"small_gross":        120,
+		"gross":              144,
+		"great_gross":        1728,
 	}
 }
 
