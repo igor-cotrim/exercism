2c3db8c222b6dd4cdaec09617ffd48a4536eaf65
Igor Cotrim Santos <igorcotrim.dev@gmail.com>
2024-01-25 15:21:41 -0300

    feat: :sparkles: resolve difference-of-squares question in Go

---
+++ go/easy/difference-of-squares.go
@@ -0,0 +1,26 @@
+// https://exercism.org/tracks/go/exercises/difference-of-squares
+package easy
+
+func SquareOfSum(n int) int {
+	var sum int
+
+	for i := 1; i <= n; i++ {
+		sum += i
+	}
+
+	return sum * sum
+}
+
+func SumOfSquares(n int) int {
+	var sum int
+
+	for i := 1; i <= n; i++ {
+		sum += i * i
+	}
+
+	return sum
+}
+
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
