0a2480db9e18fb7caec946d5ce123ae016fc62ec
Igor Cotrim Santos <igorcotrim.dev@gmail.com>
2024-02-07 12:38:37 -0300

    feat: resolve strain question in Go

---
+++ go/easy/strain.go
@@ -0,0 +1,30 @@
+// https://exercism.org/tracks/go/exercises/strain
+package easy
+
+// You will need typed parameters (aka "Generics") to solve this exercise.
+// They are not part of the Exercism syllabus yet but you can learn about
+// them here: https://go.dev/tour/generics/1
+
+func Keep[T any](items []T, pred func(T) bool) []T {
+	var kept []T
+
+	for _, item := range items {
+		if pred(item) {
+			kept = append(kept, item)
+		}
+	}
+
+	return kept
+}
+
+func Discard[T any](items []T, pred func(T) bool) []T {
+	var dis []T
+
+	for _, item := range items {
+		if !pred(item) {
+			dis = append(dis, item)
+		}
+	}
+
+	return dis
+}
