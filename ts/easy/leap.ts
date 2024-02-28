61de7bf4f8043e171e9e89e518688b4739f81ab7
Igor Cotrim Santos <igorcotrim.dev@gmail.com>
2024-02-28 10:07:18 -0300

    feat: resolve leap question in TS

---
+++ ts/easy/leap.ts
@@ -0,0 +1,13 @@
+// https://exercism.org/tracks/typescript/exercises/leap
+
+export function isLeap(year: number): boolean {
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
